7ef936445ae5fa03298ec49efc7e4fab528672f5
Pablo Clemente Maseda <paclema@gmail.com>
2020-10-17 11:36:40 +0200

    Added json import from local file on webserver

--- webserver/src/app/config-tabs/config-tabs.component.ts
+++ webserver/src/app/config-tabs/config-tabs.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import configFileData from './config/config.json';
 
 @Component({
   selector: 'app-config-tabs',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ConfigTabsComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+      console.log(configFileData);
+  }
 
   ngOnInit(): void {
   }
