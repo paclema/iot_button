2b354b6b8fafdfb71f9a918de7c961d9a02d4c3f
Pablo Clemente Maseda <paclema@gmail.com>
2020-10-20 17:18:50 +0200

    Added example of Template Driven Form

--- webserver/src/app/config-tabs/config-tabs.component.ts
+++ webserver/src/app/config-tabs/config-tabs.component.ts
@@ -11,6 +11,8 @@ export class ConfigTabsComponent implements OnInit {
   public configData;
   public errorMsg;
 
+  sleepModes = ['Deep sleep', 'Light sleep', 'Keep awake']
+
   constructor(private _configService: ConfigService) {
   }
 
