eb7a8c87c16046935892988d1140839eabbfdbcd
Pablo Clemente Maseda <paclema@gmail.com>
2020-10-20 15:23:05 +0200

    Fixed import throwError

--- webserver/src/app/config.service.ts
+++ webserver/src/app/config.service.ts
@@ -1,8 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
-import { throwError } from 'rxjs';
 
 
 @Injectable({
