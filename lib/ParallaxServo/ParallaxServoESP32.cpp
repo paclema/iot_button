d4f657ddc032184647ce404c4568364fb8443a13
Pablo Clemente <paclema@gmail.com>
2021-01-12 19:01:47 +0100

    Added ParallaxServoESP32 using ESP32Servo360 lib

---
+++ lib/ParallaxServo/ParallaxServoESP32.cpp
@@ -0,0 +1,176 @@
+#ifdef ESP32
+#include <Arduino.h>
+#include "ParallaxServoESP32.h"
+
+
+ParallaxServoESP32::ParallaxServoESP32() {
+
+}
+
+
+ParallaxServoESP32::~ParallaxServoESP32() {
+  ParallaxServoESP32::disableServo();
+}
+
+
+ParallaxServoESP32::ParallaxServoESP32(byte pinControl, byte pinFeedback) {
+  setServoControl(pinControl);
+  setServoFeedback(pinFeedback);
+}
+
+void ParallaxServoESP32::setup() {
+  if (debug){
+    Serial.print("\tcontrolPin: ");
+    Serial.print(this->controlPin);
+    Serial.print("\t");
+    Serial.print(" feedbackPin: ");
+    Serial.println(this->feedbackPin);
+  }
+  setServoControl(this->controlPin);
+  setServoFeedback(this->feedbackPin);
+
+  ParallaxServoESP32::enableServo();
+
+  // servo.adjustSignal(34, 1045);
+  // servo.calibrate();
+  servo.setSpeed(140);
+  Serial.println("Servo calibrated! Moving to 90deg");
+  servo.rotateTo(90);
+  delay(3000);
+  // servo.wait();
+  Serial.println("Servo init!");
+  delay(1000);
+
+
+}
+
+
+void ParallaxServoESP32::setServoControl(byte pin) {
+
+  this->controlPin = GPIO_ID_PIN(pin);
+  // Servo control pin attach
+
+  // this->servo.attach(this->controlPin);
+  // servo.attach(this->controlPin, this->feedbackPin);
+  ParallaxServoESP32::enableServo();
+}
+
+
+void ParallaxServoESP32::setServoFeedback(byte pin) {
+
+  this->feedbackPin = pin;
+  // Servo feedback pin attach
+  // pinMode(this->feedbackPin, INPUT);
+  // servo.attach(this->controlPin, this->feedbackPin);
+  ParallaxServoESP32::enableServo();
+}
+
+
+void ParallaxServoESP32::rotate(float degree, int threshold) {
+  this->targetAngle = degree;
+  // this->ERROR_ANGLE_OFFSET_US = threshold;
+  // servo.rotate(degree);
+  // servo.rotateTo(this->targetAngle);
+  servo.easeRotateTo(this->targetAngle);
+
+
+
+}
+
+
+bool ParallaxServoESP32::handle() {
+  // // Run pulseWidth measuring to figure out the current angle of the servo
+  // byte pin = this->feedbackPin;
+  // unsigned long tHigh = pulseIn(pin, HIGH);
+  // unsigned long tLow = pulseIn(pin, LOW);
+  // unsigned long  tCycle = tHigh + tLow;
+  // // Check if our cycle time was appropriate
+  // if (!(tCycle > 1000 && tCycle < 1200)) {
+  //   // Invalid cycle time, so try pulse measuring again
+  //   // Serial.println("Invalid cycle time");
+  //   return false;
+  // }
+  // // Calculate the duty cycle of the pulse
+  // float dutyCycle = (DUTY_SCALE) * ((float) tHigh / tCycle);
+  // float maxUnitsForCircle = UNITS_IN_FULL_CIRCLE - 1.0;
+  //
+  // // Calculate exact angle of servo
+  // this->currentAngle = maxUnitsForCircle - ((dutyCycle - DUTY_CYCLE_MIN) * UNITS_IN_FULL_CIRCLE) / ((DUTY_CYCLE_MAX - DUTY_CYCLE_MIN) + 1);
+  //
+  // // Clip current angle if we're somehow above or below range
+  // if (currentAngle < 0) {
+  //   this->currentAngle = 0;
+  // } else if (currentAngle > maxUnitsForCircle) {
+  //   this->currentAngle = maxUnitsForCircle;
+  // }
+  //
+  // // Handle quadrant wrap q1 -> q4 and q4 -> q1, to count turns
+  // if ((currentAngle < ANGLE_Q2_MIN) && (prevAngle > ANGLE_Q3_MAX)) {
+  //   this->turns += 1;
+  // } else if ((prevAngle < ANGLE_Q2_MIN) && (currentAngle > ANGLE_Q3_MAX)) {
+  //   this->turns -= 1;
+  // }
+  //
+  // // Save previous position
+  // this->prevAngle = currentAngle;
+  // this->errorAngle = targetAngle - currentAngle;
+  //
+  //
+  // // Simple P Controller
+  // int outputSpeed = errorAngle * CONSTANT_KP;
+  //
+  // if (outputSpeed > MAX_PULSE_SPEED_OFFSET_US) {
+  //   outputSpeed = MAX_PULSE_SPEED_OFFSET_US;
+  // } else if (outputSpeed < MIN_PULSE_SPEED_OFFSET_US) {
+  //   outputSpeed = MIN_PULSE_SPEED_OFFSET_US;
+  // }
+  //
+  // int offset = 0;
+  // if (errorAngle > 0) {
+  //   offset = ERROR_ANGLE_OFFSET_US;
+  // } else if (errorAngle < 0) {
+  //   offset = -1 * ERROR_ANGLE_OFFSET_US;
+  // }
+  // outputSpeed = HOLD_STILL_PULSE_SPEED_US + outputSpeed + offset;
+  // servo.writeMicroseconds(outputSpeed);
+  //
+  return true;
+
+
+
+}
+
+
+float ParallaxServoESP32::getAngle(){
+  this->currentAngle = servo.getAngle();
+  return this->currentAngle;
+}
+
+
+float ParallaxServoESP32::getAngleTarget(){
+  return this->targetAngle;
+}
+
+
+int ParallaxServoESP32::getTurns(){
+  this->turns = servo.getTurns();
+  return this->turns;
+}
+
+
+void ParallaxServoESP32::enableServo(void){
+  if(!this->servoAttached){
+    servo.attach(this->controlPin, this->feedbackPin);
+    this->servoAttached = true;
+  }
+}
+
+
+void ParallaxServoESP32::disableServo(void){
+  if(this->servoAttached){
+    this->servo.detach();
+    this->servoAttached = false;
+  }
+}
+
+#endif
---
+++ lib/ParallaxServo/ParallaxServoESP32.h
@@ -0,0 +1,91 @@
+#ifdef ESP32
+// #ifdef ParallaxServoESP32_h
+#define ParallaxServoESP32_h
+#include <Arduino.h>
+
+
+#include <ESP32Servo360.h>
+
+
+/*
+ParallaxServoESP32.h - Library to control the sensor head using a Parallax Feedback 360°
+High-Speed Servo.
+
+https://solarbotics.com/wp-content/uploads/900-00360-feedback-360-hs-servo-v1.2.pdf
+*/
+
+class ParallaxServoESP32 {
+  private:
+    ESP32Servo360 servo;                    // The Parallax Servo
+    byte controlPin;                // The servo pin
+    byte feedbackPin;               // The feedback pin (from servo)
+
+    float DUTY_SCALE = 1000;
+    unsigned long DUTY_CYCLE_MIN = 29;  // 2.9% * DUTY_SCALE
+    unsigned long DUTY_CYCLE_MAX = 971; // 9.71% * DUTY_SCALE
+    float UNITS_IN_FULL_CIRCLE = 360;   // Because 360 degrees are in a circle
+
+    // Tune the vars below for controlling how fast the servo gets to the right place and stays there
+    int ERROR_ANGLE_OFFSET_US = 50;
+    float CONSTANT_KP = 0.8;
+
+    // Max pulse 220, Min pulse 20 to each dirrection
+    int MIN_PULSE_SPEED_OFFSET_US = -5;    // Going Counter-clockwise a bit - can be smaller ( < -40)
+    int MAX_PULSE_SPEED_OFFSET_US = 5;     // Going Clockwise  a bit - can be bigger (> 40)
+    int HOLD_STILL_PULSE_SPEED_US = 1500;   // HOLD_STILL is for keeping the servo in place (no movement, don't change)
+
+    // Angles for different quadrants around the unit circle (for counting number of turns)
+    int ANGLE_Q2_MIN = 90;
+    int ANGLE_Q3_MAX = 270;
+
+    // Holds the current state
+    int targetAngle = 180;                  // The angle we want the servo to go to
+    int currentAngle = 0;                   // The angle the servo is at
+    int prevAngle = 0;                      // The last angle the servo had
+    int errorAngle = 0;                     // How off we are from the target angle
+    int turns = 0;                          // How many times we've gone around the circle
+
+    bool debug = false;
+    bool servoAttached = false;
+
+
+  public:
+    bool enabled;
+    float angleAccuracy = 0;
+    float servoSpeed = 0;
+
+    ParallaxServoESP32();
+    ParallaxServoESP32(byte pinControl, byte pinFeedback);
+    ~ParallaxServoESP32();
+
+    void setup();
+    bool handle();
+    void rotate(float degree, int threshold);
+
+    void enableServo(void);
+    void disableServo(void);
+
+    void setServoControl(byte servoPinNumber);
+    void setServoFeedback(byte servoPinNumber);
+    void setSpeedPulse(int speedPulse){
+      this->MIN_PULSE_SPEED_OFFSET_US = -speedPulse;
+      this->MAX_PULSE_SPEED_OFFSET_US = speedPulse;
+    };
+
+    float getAngle();
+    float getAngleTarget();
+    int getTurns();
+
+
+    void setDebug(bool d){ this->debug = d; };
+    bool isDebug(void){ return this->debug; };
+
+    bool isRunning(void){ return this->servo.busy(); };
+
+    bool isAttached(void){ return this->servoAttached; };
+
+
+};
+
+
+#endif
