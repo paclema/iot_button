1ae216993db0fec61700266ca603e0c799f8a970
Pablo Clemente <paclema@gmail.com>
2021-02-11 14:14:19 +0100

    Added PeopleCounter class

---
+++ lib/PeopleCounter/PeopleCounter.cpp
@@ -0,0 +1,242 @@
+#include "PeopleCounter.h"
+
+
+
+PeopleCounter::PeopleCounter(void) {
+  this->nameConfigObject = "PeopleCounter";
+};
+
+
+PeopleCounter::PeopleCounter(String name) {
+  this->nameConfigObject = name;
+};
+
+
+void PeopleCounter::parseWebConfig(JsonObjectConst configObject){
+
+  // JsonObject received:
+  // serializeJsonPretty(configObject, Serial);
+
+  // Disable distance sensors:
+  // if (this->peopleCounterInitialized) PeopleCounter::disableDistSensors();
+
+
+  // PeopleCounter IWebConfig object:
+  this->debug = configObject["debug"] | false;
+
+
+  // VL53L1X with ROI sensor:
+  if (configObject["vl53l1x"]["enabled"]){
+    sensor.setEnable(configObject["ROI"]["enabled"] | false);
+    sensor.setDebug(configObject["vl53l1x"]["debug"] | false);
+    sensor.setTimeBudget(configObject["vl53l1x"]["time_budget_ms"]);
+  }
+
+  sensor.parseWebConfig(configObject);
+
+
+  // Setup again PeopleCounter:
+  // if (this->peopleCounterInitialized) PeopleCounter::setupDistSensors();
+};
+
+
+void PeopleCounter::enablePeopleCounterServices(void){
+  Serial.println("--- PeopleCounter: ");
+  Serial.printf("   - Debug: %s\n", this->debug ? "true" : "false");
+
+  sensor.setName("vl53l1x");
+  sensor.setType("VL53L1X_ROI");
+  sensor.setup();
+
+
+  this->peopleCounterInitialized = true;
+};
+
+
+void PeopleCounter::disableDistSensors(void){
+
+  // Serial.printf("Disabling %d sensors:\n", distanceSensors.size());
+  // DistSensor *sensorTemp ;
+  // for(int i = 0; i < distanceSensors.size(); i++){
+  //   sensorTemp = distanceSensors.get(i);
+  //   sensorTemp->disableSensor();
+  //   if (this->debug) Serial.printf("\t-> %s disabled\n", sensorTemp->getName().c_str());
+  // }
+
+  sensor.disableSensor();
+
+
+};
+
+
+void PeopleCounter::setupDistSensors(void){
+
+};
+
+void PeopleCounter::loop(void){
+
+  bool readStatus = sensor.sensorRead2Roi();
+
+  // CHECK GESTURE USING ALGORITHM:
+  if (sensor.distance[0] < rangeThresholdCounter_mm) statusFront = 2;
+  else statusFront = 0;
+  if (sensor.distance[1] < rangeThresholdCounter_mm) statusBack = 1;
+  else statusBack = 0;
+
+  statusPersonNow = statusFront + statusBack;
+  if (statusPersonLast != statusPersonNow){
+    statusPersonIndex++;
+    statusPerson[statusPersonIndex] = statusPersonNow;
+    statusPersonLast = statusPersonNow;
+  } else if (statusPersonNow == 0 && statusPersonLast == 0){
+    statusPersonIndex = 0;
+  }
+
+  // Serial.printf("Current statusPersonNow: %d - statusPersonIndex: %d - cnt: %d\n", statusPersonNow, statusPersonIndex, cnt);
+  
+  // Decode person gesture if statusPerson counter reaches enough information:
+  if ( statusPersonIndex == 4 || (statusPersonIndex == 2 && statusPersonNow == 0)) {
+
+    PeopleCounterGesture newGesture;
+    int statusPersonTotal = 0;
+    for (int i=0; i<STATUS_PERSON_ARRAY_SIZE; i++){
+      statusPersonTotal += statusPerson[i];
+    }
+
+    switch (statusPersonTotal){
+    case 6:
+      if (statusPerson[1] == 2){
+        cnt++;
+        newGesture = PERSON_ENTERS;
+      } else if (statusPerson[1] == 1) {
+        cnt--;
+        newGesture = PERSON_LEAVES;
+      } else {
+        newGesture = ERROR_DETECTING_PERSON;
+      }
+      break;
+    case 2:
+    case 7:
+      newGesture = PERSON_TRY_TO_ENTER;
+      break;
+    case 1:
+    case 5:
+      newGesture = PERSON_TRY_TO_LEAVE;
+      break;
+    default:
+      newGesture = ERROR_DETECTING_PERSON;
+      break;
+    }
+    Serial.println();
+
+    currentGesture = newGesture;
+    PeopleCounter::notifyChange(newGesture);
+    statusPersonIndex = 0;
+    statusPersonNow = 0;
+    statusPersonLast = 0;
+    for (int i=0; i<STATUS_PERSON_ARRAY_SIZE; i++){
+      statusPerson[i] = 0;
+    }
+  } else {
+    currentGesture = NO_PERSON_DETECTED;
+  }
+
+
+  if ( sensor.distance[0] < rangeThresholdCounter_mm || sensor.distance[1] < rangeThresholdCounter_mm) { timeMark = millis(); }
+  if (millis() - timeMark < 500) {
+
+    // Serial.print("Distances: ");
+    // Serial.print(sensor.distance[0]);
+    // Serial.print(", ");
+    // Serial.print(sensor.distance[1]);
+    // Serial.print(": counter ---> ");
+		// Serial.print((cnt));	
+		// Serial.println();	
+    // timeMark = millis(); 
+
+    String msgStatusPerson = "{ ";
+    for (int i=0; i<STATUS_PERSON_ARRAY_SIZE; i++){
+      msgStatusPerson += String(statusPerson[i]);
+      if (i != 4) msgStatusPerson += ", ";
+    }
+    msgStatusPerson += " }";
+    Serial.println(" - statusPerson[5]: " + msgStatusPerson + " - currentGesture: " + currentGesture);
+
+    String topic_pub = "/iot-door/data/statusPerson";
+    String msg_pub = "statusPerson[5]:" + msgStatusPerson;
+    mqttClient->publish(topic_pub.c_str(), msg_pub.c_str(), msg_pub.length());
+    }
+
+
+
+
+};
+
+
+void PeopleCounter::printStatus(void){
+  // From sensorDistance vl53l1x:
+  // if (this->sensorDistance.debug){
+  //   // Serial.print(this->sensorDistance.getSensorRange());
+  //   this->sensorDistance.printSensorStatus();
+  // }
+};
+
+
+void PeopleCounter::notifyChange(PeopleCounterGesture gesture){
+
+  String msgGesture;
+  switch (gesture){
+  case PERSON_ENTERS:
+    msgGesture = "Person enters";
+    break;
+  case PERSON_LEAVES:
+    msgGesture = "Person leaves";
+    break;
+  case PERSON_TRY_TO_ENTER:
+    msgGesture = "Person try to enter";
+    break;
+  case PERSON_TRY_TO_LEAVE:
+    msgGesture = "Person try to leave";
+    break;
+  case ERROR_DETECTING_PERSON:
+    msgGesture = "Error detecting detecting person";
+    break;
+  default:
+    msgGesture = "PeopleCounterGesture not implemented";
+    break;
+  }
+  Serial.print(msgGesture);
+  Serial.printf(" - cnt: %d", cnt);
+
+
+  String msgStatusPerson = "{ ";
+  for (int i=0; i<STATUS_PERSON_ARRAY_SIZE; i++){
+    msgStatusPerson += String(statusPerson[i]);
+    if (i != 4) msgStatusPerson += ", ";
+  }
+  msgStatusPerson += " }";
+  Serial.println(" - statusPerson[5]: " + msgStatusPerson + " - currentGesture: " + currentGesture);
+
+
+  // Blink LED_BUILTIN once for 100ms:
+  ledOn.once_ms(0, PeopleCounter::blink , true);
+  ledOff.once_ms(100, PeopleCounter::blink, false);
+
+  
+  // Notify via MQTT:
+  String topic_pub = "/iot-door/data/PeopleCounterGesture";
+  String msg_pub = String(gesture);
+  mqttClient->publish(topic_pub.c_str(), msg_pub.c_str(), msg_pub.length());
+
+  topic_pub = "/iot-door/data/PeopleCounterGesture/decoded";
+  msg_pub = msgGesture;
+  mqttClient->publish(topic_pub.c_str(), msg_pub.c_str(), msg_pub.length());
+
+  topic_pub = "/iot-door/data";
+  msg_pub = "{ \"people_count\": " + String(this->cnt) + " }";
+  mqttClient->publish(topic_pub.c_str(), msg_pub.c_str(), msg_pub.length());
+
+}
+
+
+
---
+++ lib/PeopleCounter/PeopleCounter.h
@@ -0,0 +1,104 @@
+#ifndef PeopleCounter_H
+#define PeopleCounter_H
+
+#include <PubSubClient.h>
+#include "Ticker.h"
+
+#define JSON_MAX_SIZE_LIST 6
+
+// #include <LinkedList.h>
+// Using AsyncWebServer LinkedList lib can not be used because there is a class
+// using the same name. For that reason, for now we use SimpleList until we fix
+// this using namespace for example.
+// #include <SimpleList.h>
+#include "IWebConfig.h"
+
+// #include "PeopleCounterMotor.h"
+// #include "DistSensor.h"
+// #include "DistSensorFactory.h"
+// ParallaxServo class can work with ESP8266 or ESP32:
+// #include "ParallaxServo.h"
+
+// #ifdef ESP32
+// // To use paclema/ESP32Servo360:
+// #include "ParallaxServoESP32.h"
+// // #elif defined(ESP8266)
+// #endif
+
+#include "DistSensorVL53L1XROI.h"
+
+
+#define STATUS_PERSON_ARRAY_SIZE 5
+
+typedef int8_t PeopleCounterGesture;
+#define NO_PERSON_DETECTED              ((PeopleCounterGesture)   0)
+#define PERSON_ENTERS                   ((PeopleCounterGesture)   1)
+#define PERSON_LEAVES                   ((PeopleCounterGesture)   2)
+#define PERSON_TRY_TO_ENTER             ((PeopleCounterGesture)   3)
+#define PERSON_TRY_TO_LEAVE             ((PeopleCounterGesture)   4)
+
+#define ERROR_DETECTING_PERSON          ((PeopleCounterGesture)  -1)
+#define PERSON_NOT_FULL_DETECTED        ((PeopleCounterGesture)  -2)
+
+
+
+class PeopleCounter: public IWebConfig {
+
+private:
+
+  bool debug = false;
+  bool peopleCounterInitialized  = false;
+  
+  int statusPerson[STATUS_PERSON_ARRAY_SIZE];
+  int statusPersonNow = 0, statusPersonLast = 0, statusPersonIndex = 0;
+  int statusFront = 0;
+  int statusBack = 0;
+
+  int  cnt = 0;
+  long timeMark = 0;
+
+  int rangeThresholdCounter_mm = 1200;
+
+  PeopleCounterGesture currentGesture = NO_PERSON_DETECTED;
+  PubSubClient *mqttClient;
+  Ticker ledOn, ledOff;
+
+public:
+
+  DistSensorVL53L1XROI sensor;
+
+  PeopleCounter(void);
+  PeopleCounter(String name);
+
+  void parseWebConfig(JsonObjectConst configObject);
+
+  void enablePeopleCounterServices(void);
+  void disableDistSensors(void);
+  void setupDistSensors(void);
+  void loop(void);
+
+  void notifyChange(PeopleCounterGesture);
+  void setMQTTClient(PubSubClient *client){ mqttClient = client; }
+
+  void printStatus(void);
+
+  String getPeopleCount(void){ return String(cnt); };
+  String getDistZone(int i){ return String(sensor.distance[i]); };
+  String getStatusFront(){ return String(statusFront); };
+  String getStatusBack(){ return String(statusBack); };
+  String getStatusPersonNow(){ return String(statusPersonNow); };
+  String getCurrentGesture(){ return String(currentGesture); };
+
+
+  void setDebug(bool d){ this->debug = d; };
+  bool isDebug(void){ return this->debug; };
+
+
+  static void blink(bool status){
+    pinMode(LED_BUILTIN, OUTPUT);
+    // Serial.printf("Blinking to %d", status);
+    digitalWrite(LED_BUILTIN, !status); 
+  };
+
+};
+#endif
